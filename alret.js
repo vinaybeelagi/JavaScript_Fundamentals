c6a2f38cb02d7cd7c3c3546d77fe9b80556c2abc
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-05 17:26:39 +0530

    A variable,The variable for our planet

--- alret.js
+++ alret.js
@@ -1,6 +1,11 @@
 // alert("Hello,I am Javascript")
 
-let admin,name;
- name = "john";
-admin = name ;
-alert(admin);
+// let admin,name;
+//  name = "john";
+// admin = name ;
+// alert(admin);
+
+//  task-The variable for our planet
+const myPlanetName = "Earth";
+let currentUserName = "devid";
+
