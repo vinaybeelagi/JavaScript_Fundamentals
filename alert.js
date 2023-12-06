247de48d48308e6d92d99b7bd76c6c203bcdf642
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-06 13:35:29 +0530

    Fix the addition

--- alert.js
+++ alert.js
@@ -28,33 +28,45 @@
 // alert(name);
 
 // task-operators-The postfix and prefix forms
-let a = 1, b = 1;
+// let a = 1, b = 1;
 
-let c = ++a; // 2
-let d = b++; // 1
-// output : a=2 b=2 c=2 d=1
+// let c = ++a; // 2
+// let d = b++; // 1
+// // output : a=2 b=2 c=2 d=1
 
-// task-Assignment result
-let a = 2;
+// // task-Assignment result
+// let a = 2;
 
-let x = 1 + (a *= 2);
-// o/p: a=4 x=5
+// let x = 1 + (a *= 2);
+// // o/p: a=4 x=5
 
-// Type conversions
-"" + 1 + 0  // '10'
-"" - 1 + 0  //-1
-true + false //1
-6 / "3" // 2
-"2" * "3" //6
-4 + 5 + "px" //'9px'
-"$" + 4 + 5 //"$45"
-"4" - 2 // 2
-"4px" - 2 // NaN
-"  -9  " + 5 //"-9 5"
-"  -9  " - 5 //-14
-null + 1 // 1
-undefined + 1 //NaN
-" \t \n" - 2 // NaN
+// // Type conversions
+// "" + 1 + 0  // '10'
+// "" - 1 + 0  //-1
+// true + false //1
+// 6 / "3" // 2
+// "2" * "3" //6
+// 4 + 5 + "px" //'9px'
+// "$" + 4 + 5 //"$45"
+// "4" - 2 // 2
+// "4px" - 2 // NaN
+// "  -9  " + 5 //"-9 5"
+// "  -9  " - 5 //-14
+// null + 1 // 1
+// undefined + 1 //NaN
+// " \t \n" - 2 // NaN
+
+// Fix the addition
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(a + b); // 12
+// After fix:
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?",2);
+alert(a+b); 
+
+
 
 
 
