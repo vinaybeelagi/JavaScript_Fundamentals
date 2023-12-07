f46932e3c324756ae95946708c9dbe7fc3bbb80f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-07 20:48:59 +0530

    Function pow(x,n)

--- function.js
+++ function.js
@@ -20,19 +20,39 @@
 // //   Ans: no difference
 
 // Rewrite the function using '?' or '||'
-function checkAge(age){
-    return(age>18) ? true:confirm('Did parents allow you?');
+// function checkAge(age){
+//     return(age>18) ? true:confirm('Did parents allow you?');
+// }
+
+// function checkAge(age) {
+//     return (age > 18) || confirm('Did parents allow you?');
+//   }
+
+// // task- Function min(a, b)
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+
+// task-Function pow(x,n)
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
 }
 
-function checkAge(age) {
-    return (age > 18) || confirm('Did parents allow you?');
-  }
+let x = prompt("x?", '');
+let n = prompt("n?", '');
 
-// task- Function min(a, b)
-function min(a, b) {
-    if (a < b) {
-      return a;
-    } else {
-      return b;
-    }
-  }
+if (n < 1) {
+  alert(`Power ${n} is not supported, use a positive integer`);
+} else {
+  alert( pow(x, n) );
+}
