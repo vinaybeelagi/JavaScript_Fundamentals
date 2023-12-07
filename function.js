b3654315733db3326b7af2e8a139e0aac4dea894
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-07 18:09:02 +0530

    Rewrite the function using ? or ||

--- function.js
+++ function.js
@@ -1,20 +1,29 @@
-// Task-Is "else" required?
-// example-1
-function checkAge(age) {
-    if (age > 18) {
-      return true;
-    } else {
-      // ...
-      return confirm('Did parents allow you?');
-    }
-  }
-//   example-2
-function checkAge(age) {
-    if (age > 18) {
-      return true;
-    }
-    // ...
-    return confirm('Did parents allow you?');
-  }
+// //Topic-Functions: Task-Is "else" required?
+// // example-1
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     } else {
+//       // ...
+//       return confirm('Did parents allow you?');
+//     }
+//   }
+// //   example-2
+// function checkAge(age) {
+//     if (age > 18) {
+//       return true;
+//     }
+//     // ...
+//     return confirm('Did parents allow you?');
+//   }
 
-//   Ans: no difference
+// //   Ans: no difference
+
+// Rewrite the function using '?' or '||'
+function checkAge(age){
+    return(age>18) ? true:confirm('Did parents allow you?');
+}
+
+function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+  }
