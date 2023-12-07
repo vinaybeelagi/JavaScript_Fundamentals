f414d08d5ef8fc2145c32ca49eedfd53fdf84eff
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-07 14:01:18 +0530

    Output prime numbers

--- alert.js
+++ alert.js
@@ -171,12 +171,21 @@
 //   }
 // }
 
-// Repeat until the input is correct
-let num;
-do {
-  num = prompt("Enter a number greater than 100?",0);
-}while(num<=100 && num);
+// task-Repeat until the input is correct
+// let num;
+// do {
+//   num = prompt("Enter a number greater than 100?",0);
+// }while(num<=100 && num);
 
+//task-Output prime numbers
+let n = 10;
+for (let i = 2; i <= n; i++) { 
+nextPrime:
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  } 
+  alert( i );
+}
 
 
 
